21f92cd0d7ad32c71a1c5091efac27540e2fbcfd
quangpa90 <56436244+quangpa90@users.noreply.github.com>
2022-02-26 21:53:10 +0700

    Update settings.py

--- taplamweb/taplamweb/settings.py
+++ taplamweb/taplamweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y&vli=74k4axo+e)xrx!h+wlnpv)le8oi7lh3=a_@b@o2f$gkr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,4 +123,4 @@ STATIC_URL = 'http://www.taisaolaithe.herokuapp.com/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #activate django-heroku
-django_heroku.setting(locals())
+django_heroku.settings(locals())
